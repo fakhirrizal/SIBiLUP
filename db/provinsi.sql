5a72e0ecfccf23762db9790ae1e8cf0a0251b842
fakhi_rizal <bokir.rizal@gmail.com>
2019-09-15 10:07:36 +0700

    tabel provinsi & tabel kabupaten

---
+++ db/provinsi.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 15 Sep 2019 pada 05.06
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sibilup`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `provinsi`
+--
+
+CREATE TABLE `provinsi` (
+  `id_provinsi` int(5) NOT NULL,
+  `nm_provinsi` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `provinsi`
+--
+
+INSERT INTO `provinsi` (`id_provinsi`, `nm_provinsi`) VALUES
+(11, 'ACEH'),
+(12, 'SUMATERA UTARA'),
+(13, 'SUMATERA BARAT'),
+(14, 'RIAU'),
+(15, 'JAMBI'),
+(16, 'SUMATERA SELATAN'),
+(17, 'BENGKULU'),
+(18, 'LAMPUNG'),
+(19, 'BANGKA BELITUNG'),
+(21, 'KEPULAUAN RIAU'),
+(31, 'DKI JAKARTA'),
+(32, 'JAWA BARAT'),
+(33, 'JAWA TENGAH'),
+(34, 'DI YOGYAKARTA'),
+(35, 'JAWA TIMUR'),
+(36, 'BANTEN'),
+(51, 'BALI'),
+(52, 'NUSA TENGGARA BARAT'),
+(53, 'NUSA TENGGARA TIMUR'),
+(61, 'KALIMANTAN BARAT'),
+(62, 'KALIMANTAN TENGAH'),
+(63, 'KALIMANTAN SELATAN'),
+(64, 'KALIMANTAN TIMUR'),
+(65, 'KALIMANTAN UTARA'),
+(71, 'SULAWESI UTARA'),
+(72, 'SULAWESI TENGAH'),
+(73, 'SULAWESI SELATAN'),
+(74, 'SULAWESI TENGGARA'),
+(75, 'GORONTALO'),
+(76, 'SULAWESI BARAT'),
+(81, 'MALUKU'),
+(82, 'MALUKU UTARA'),
+(91, 'PAPUA BARAT'),
+(94, 'PAPUA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  ADD PRIMARY KEY (`id_provinsi`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  MODIFY `id_provinsi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
